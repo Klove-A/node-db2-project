b1a1ed5b8da485314fbf93479858d2fc6bb09902
Klove Adams <kloveadams@gmail.com>
2021-10-05 16:19:55 -0400

    Wrote cars-models.js will edit if needed

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,21 @@
-const getAll = () => {
-  // DO YOUR MAGIC
-}
+const db = require("../../data/db-config");
 
-const getById = () => {
-  // DO YOUR MAGIC
-}
+const getAll = () => {
+  return db("cars");
+};
+
+const getById = (id) => {
+  return db("cars").where("id", id).first();
+};
 
 const create = () => {
-  // DO YOUR MAGIC
-}
+  const [id] = await db("cars").insert(post);
+  return getById(id);
+};
+
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};

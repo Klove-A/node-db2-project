3b7b8a201d55d0e8d4213a62a16af328ce61df7c
Klove Adams <kloveadams@gmail.com>
2021-10-05 18:42:49 -0400

    added seed with valid vins

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,17 @@
-// STRETCH
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert([
+    {
+      vin: "1N4AL3AP4EC445507",
+      make: "test-make-1",
+      model: "test-model-1",
+      mileage: 123,
+    },
+    {
+      vin: "1M2P267CXTM064650",
+      make: "test-make-2",
+      model: "test-model-2",
+      mileage: 123,
+    },
+  ]);
+};

3e04c1ac456e3e8b3985e4664cea1ce93a32f416
Klove Adams <kloveadams@gmail.com>
2021-10-05 16:31:12 -0400

    built checkCarId can't test yet

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,25 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+Car = require("./cars-model");
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const { id } = req.params.id;
+    const car = await Car.getAll(id);
+    if (!car) {
+      next({status: 404, message: `car with id ${id} is not found`})
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
